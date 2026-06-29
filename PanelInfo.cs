31bf1af36f0e586ffd3b47109991d6bb9e5a4053
db1096 <luuson1096@gmail.com>
2026-06-29 16:33:23 +0700

    Refactor PanelInfo class properties
    Removed default value for ScanTime and UpdateTime properties.

--- PanelInfo.cs
+++ PanelInfo.cs
@@ -3,10 +3,6 @@ using System;
 internal class PanelInfo
 {
     public int PNL { get; set; }
-
     public int Defect { get; set; }
-
-    public string ScanTime { get; set; } = "";
-
-    public DateTime UpdateTime { get; set; } = DateTime.Now;
+    public string ScanTime { get; set; }
 }
